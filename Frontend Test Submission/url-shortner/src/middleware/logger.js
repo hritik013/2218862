9b74c851299c71ac8ae67ab47a3791386a7e56ff
Hritik rawat <148574633+hritik013@users.noreply.github.com>
2025-07-15 12:59:27 +0530

    Update logger.js

--- Frontend Test Submission/url-shortner/src/middleware/logger.js
+++ Frontend Test Submission/url-shortner/src/middleware/logger.js
@@ -1,5 +1,6 @@
 const LOG_API = "http://20.244.56.144/evaluation-service/logs";
 
+const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJyYXdhdGhyaXRpazE5QGdtYWlsLmNvbSIsImV4cCI6MTc1MjU2NDU3NiwiaWF0IjoxNzUyNTYzNjc2LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiMDEzMTkzYmItOTNkMC00YjRjLThhNjctNTA4Mzg4YzA3OTE5IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoiaHJpdGlrIHJhd2F0Iiwic3ViIjoiNjg3MjgyMTktMzYzZS00N2UwLTg0ZTUtNmY0NWU0NGViNDlkIn0sImVtYWlsIjoicmF3YXRocml0aWsxOUBnbWFpbC5jb20iLCJuYW1lIjoiaHJpdGlrIHJhd2F0Iiwicm9sbE5vIjoiMjIxODg2MiIsImFjY2Vzc0NvZGUiOiJRQWhEVXIiLCJjbGllbnRJRCI6IjY4NzI4MjE5LTM2M2UtNDdlMC04NGU1LTZmNDVlNDRlYjQ5ZCIsImNsaWVudFNlY3JldCI6InVyTUZiUWhucXFnbmRVU3UifQ.xx68nr8P8itFZ0YhCOzioelFRKysarWICNE4trkZ4m0";
 
 export const Log = async (stack, level, logPackage, message) => {
   const payload = {
@@ -14,18 +15,12 @@ export const Log = async (stack, level, logPackage, message) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Authorization": `Bearer ${ACCESS_TOKEN}`,
       },
       body: JSON.stringify(payload),
     });
 
-    const result = await res.json();
-
-    if (!res.ok) {
-      console.error(" Logging failed:", result);
-    } else {
-      console.log(" Log created:", result);
-    }
+    
   } catch (err) {
-    console.error("Error sending log:", err);
   }
 };
